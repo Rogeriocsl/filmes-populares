1ecd0b7a92503e082658ef809f0ccaefdbfacb1d
rogeriocsl <carlosrog1414@hotmail.com>
2023-10-20 22:15:50 -0300

    Conectando a api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [filmes, setFilmes] = useState([]);
+
+  const fetchFilmes = async () => {
+    const response = await axios.get(
+      "https://api.themoviedb.org/3/movie/popular?api_key=d6a4e09d194a34b300a7bd1b4d36353f"
+    );
+    setFilmes(response.data.results);
+  };
+
+  useEffect(() => {
+    fetchFilmes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Ano</th>
+            <th>Gênero</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filmes &&
+            filmes.map((filme) => (
+              <tr key={filme.id}>
+                <td>{filme.title}</td>
+                <td>{filme.release_date}</td>
+                <td>
+                  {filme.genres && filme.genres.length > 0
+                    ? filme.genres.map((genre) => genre.name).join(", ")
+                    : "Não há informações de gênero disponíveis."}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
